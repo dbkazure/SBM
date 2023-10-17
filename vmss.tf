3f3485fe42cb052c7b0b388b2efaccfa694e6015
Dhan <bkdhan@outlook.com>
2023-10-17 19:39:11 +0530

    Terraform create vmss task

--- vmss.tf
+++ vmss.tf
@@ -1,6 +1,7 @@
 # a.	A single web page(HTML) hosted on an auto scaling group.(Default nodes =2). Web servers should  be in Compute Subnet. (Refer annex 1 - (1) for HTML file content.)
 # b.	The web service should be behind an Load Balancer, place this load balancer in the DMZ subnet. Load balance with sticky IP algo.
 # c.	A security group that allows only 1 local IP address to ssh into the vm instances.
+#    Only IP : 172.33.128.50 is allowed to SSH to the VM Instances.
 
 #Provider
 provider "azurerm" {
@@ -38,31 +39,127 @@ resource "azurerm_subnet" "dmz" {
   depends_on           = [azurerm_virtual_network.myvnet]
 }
 
+## NSG for VMSS
 resource "azurerm_network_security_group" "myvmssnsg" {
   name                = "myvmssnsg"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
 }
 
-resource "azurerm_network_security_rule" "nsgrule" {
-  name                        = "example-allow"
+resource "azurerm_network_security_rule" "nsgInrule1" {
+  name                        = "SSH-allow"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = "*"
+  source_address_prefix       = "172.33.128.50"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.myrg.name
+  network_security_group_name = azurerm_network_security_group.myvmssnsg.name
+
+}
+
+resource "azurerm_network_security_rule" "nsgInrule2" {
+  name                        = "LB-allow"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "AzureLoadBalancer"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.myrg.name
   network_security_group_name = azurerm_network_security_group.myvmssnsg.name
 }
 
 
+resource "azurerm_network_security_rule" "nsgoutrule1" {
+  name                        = "SSH-allow"
+  priority                    = 200
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "Internet"
+  resource_group_name         = azurerm_resource_group.myrg.name
+  network_security_group_name = azurerm_network_security_group.myvmssnsg.name
+
+}
 
 
+##Azure Load balancer
 
-#VM Scale Set
+#Load balance public IP
+#resource "azurerm_public_ip" "pub_ip" {
+# name                = "lbpubip"
+#  location            = azurerm_resource_group.myrg.location
+# resource_group_name = azurerm_resource_group.myrg.name
+# allocation_method   = "Static"
+# sku = "Standard"
+#}
+
+#Load balancer config
+
+resource "azurerm_lb" "vmss_lb" {
+  name                = "vmsslb"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+  sku                 = "Standard"
+  sku_tier            = "Regional"
+  frontend_ip_configuration {
+    name                          = "frontend-ip"
+    subnet_id                     = azurerm_subnet.dmz.id
+    private_ip_address_allocation = "Dynamic"
+    private_ip_address_version    = "IPv4"
+  }
+
+  depends_on = [
+    azurerm_subnet.dmz
+  ]
+}
+
+# Backend pool
+resource "azurerm_lb_backend_address_pool" "vmsspool" {
+  loadbalancer_id = azurerm_lb.vmss_lb.id
+  name            = "vmsspool"
+  depends_on = [
+    azurerm_lb.vmss_lb
+  ]
+}
+
+#Health Probe
+resource "azurerm_lb_probe" "Probe1" {
+  #resource_group_name = azurerm_resource_group.myrg.name
+  loadbalancer_id = azurerm_lb.vmss_lb.id
+  name            = "Probe1"
+  port            = 80
+  protocol        = "Tcp"
+  depends_on = [
+    azurerm_lb.vmss_lb
+  ]
+}
+
+#Load Balancing Rule
+resource "azurerm_lb_rule" "Rule1" {
+  #resource_group_name            = azurerm_resource_group.myrg.name
+  loadbalancer_id                = azurerm_lb.vmss_lb.id
+  name                           = "Rule1"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "frontend-ip"
+  probe_id                       = azurerm_lb_probe.Probe1.id
+  load_distribution              = "SourceIP"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.vmsspool.id]
+}
+
+
+##VM Scale Set
 
 resource "azurerm_linux_virtual_machine_scale_set" "myvmss" {
   name                            = "my-vmss"
@@ -73,17 +170,17 @@ resource "azurerm_linux_virtual_machine_scale_set" "myvmss" {
   admin_username                  = "adminuser"
   admin_password                  = "!tempP@ss"
   disable_password_authentication = false
-
- 
-
+  upgrade_mode                    = "Automatic"
   network_interface {
-    name    = "nic"
-    primary = true
+    name                      = "nic"
+    primary                   = true
+    network_security_group_id = azurerm_network_security_group.myvmssnsg.id
 
     ip_configuration {
-      name      = "internal"
-      primary   = true
-      subnet_id = azurerm_subnet.compute.id
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.compute.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.vmsspool.id]
     }
   }
 
@@ -97,15 +194,16 @@ resource "azurerm_linux_virtual_machine_scale_set" "myvmss" {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
-   connection {
+  connection {
     type     = "ssh"
     user     = "adminuser"
     password = "!tempP@ss"
     host     = azurerm_linux_virtual_machine_scale_set.myvmss.id
-    
+
   }
 }
 
+## Online storage for script
 resource "azurerm_storage_account" "scriptstore" {
   name                     = "scriptstore202310171"
   resource_group_name      = azurerm_resource_group.myrg.name
@@ -118,9 +216,9 @@ resource "azurerm_storage_container" "scriptfile" {
   name                  = "scriptfile"
   storage_account_name  = "scriptstore202310171"
   container_access_type = "blob"
-  depends_on=[
+  depends_on = [
     azurerm_storage_account.scriptstore
-    ]
+  ]
 }
 
 # Uploading script to blob
@@ -131,17 +229,16 @@ resource "azurerm_storage_blob" "script" {
   storage_container_name = "scriptfile"
   type                   = "Block"
   source                 = "script.sh"
-   depends_on=[azurerm_storage_container.scriptfile]
+  depends_on             = [azurerm_storage_container.scriptfile]
 }
 
-// Here we are applying the custom script extension on the 
-// virtual machine scale set
+## Custom script extenstion for the VMSS
 resource "azurerm_virtual_machine_scale_set_extension" "scaleset_extension" {
-  name                 = "scaleset-extension"
-  virtual_machine_scale_set_id   = azurerm_linux_virtual_machine_scale_set.myvmss.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.1"
+  name                         = "scaleset-extension"
+  virtual_machine_scale_set_id = azurerm_linux_virtual_machine_scale_set.myvmss.id
+  publisher                    = "Microsoft.Azure.Extensions"
+  type                         = "CustomScript"
+  type_handler_version         = "2.1"
   depends_on = [
     azurerm_storage_blob.script
   ]
